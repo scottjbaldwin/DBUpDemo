9c79ee90eaaea2e8270d870cbaf4a9b9ba94f5d4
scott.baldwin <scott.baldwin@purple.telstra.com>
2023-11-21 14:44:21 +1100

    Qoute all column names in SQL

--- src/dbupdemo/Schema/Script0001 - Basic Database.sql
+++ src/dbupdemo/Schema/Script0001 - Basic Database.sql
@@ -1,32 +1,33 @@
 USE `dbupdemo`
 
 CREATE TABLE IF NOT EXISTS `dbupdemo`.`Contacts` (
-    ContactId INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    GivenName VARCHAR(100),
-    Surname VARCHAR(100),
-    ContactType varchar(20),
-    CreatedDate DATETIME NOT NULL,
-    LastModifiedDate DATETIME NOT NULL,
-    Notes VARCHAR(500),
-  PRIMARY KEY (ContactId),
+    `ContactId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `GivenName` VARCHAR(100),
+    `Surname` VARCHAR(100),
+    `ContactType` varchar(20),
+    `CreatedDate` DATETIME NOT NULL,
+    `LastModifiedDate` DATETIME NOT NULL,
+    `Notes` VARCHAR(500),
+  PRIMARY KEY (`ContactId`),
 )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `dbupdemo`.`Address` (
-	AddressId INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	Street nvarchar(100),
-	City nvarchar(50),
-	State nvarchar(50),
-	Country nvarchar(50),
-	Postcode nvarchar(5)
+	`AddressId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`Street` nvarchar(100),
+	`City` nvarchar(50),
+	`State` nvarchar(50),
+	`Country` nvarchar(50),
+	`Postcode` nvarchar(5),
+	PRIMARY KEY (`AddressId`)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `dbupdemo`.`ContactAddress` (
-	ContactAddressId INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	ContactId uniqueidentifier,
-	AddressId uniqueidentifier,
-	AddressType int, -- 1 = postal, 2 = billing
-  PRIMARY KEY (ContactAddressId)
+	`ContactAddressId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`ContactId` uniqueidentifier,
+	`AddressId` uniqueidentifier,
+	`AddressType` int, -- 1 = postal, 2 = billing
+  PRIMARY KEY (`ContactAddressId`)
 )
 ENGINE = InnoDB;
